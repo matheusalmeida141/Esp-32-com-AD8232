d20415af5452a0ade676929d97b1f3439c35b0ce
matheusalmeida141 <117413794+matheusalmeida141@users.noreply.github.com>
2022-11-08 20:47:04 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,7 +5,7 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D))
